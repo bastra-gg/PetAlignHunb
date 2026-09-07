2ea63583251e43df192024dd298fa85a31be4343
bastra-gg <po4ta.dlya.vsego69@gmail.com>
2026-09-07 17:03:26 +0200

    Add BGS Legacy reference data

---
+++ BGS_Legacy_Hub_v0_1/BGS_Data.lua
@@ -0,0 +1,13 @@
+return {
+Overworld={currency="Coins/Gems",unlock="free",islands={"Floating Island 85m","Space 518m","Twilight 1134m","Skylands 2105m","Void 3208m","Zen 4065m"},eggs={
+{"Common Egg",250,"Coins"},{"Spotted Egg",3200,"Coins"},{"Ice Shard Egg",250,"Gems"},{"Spikey Egg",35000,"Coins"},
+{"Magma Egg",2000,"Gems"},{"Crystal Egg",17500,"Gems"},{"Lunar Egg",50000,"Gems"},
+{"Void Shard Egg",200000,"Gems"},{"Hell Egg",300000,"Gems"},{"Nightmare Egg",350000,"Gems"},{"Rainbow Egg",650000,"Gems"}}},
+CandyLand={currency="Candy",unlock="2.5M Coins",islands={"Gumdrop Island","Rewards Island","Sugar Island","Candy Island","Sweet Island"},eggs={{"Ice Cream Egg",400},{"Gummy Egg",75000},{"Slushy Egg",200000},{"Jelly Egg",300000},{"Dominus Egg",325000}}},
+ToyLand={currency="Blocks",unlock="50M Coins",islands={"Block Island","Block Rewards","Toy Isle","Teddy Island","Treasure Isle"},eggs={{"Wind-Up Egg",425},{"Block Egg",75000},{"Toy Egg",175000},{"Rubber Egg",200000},{"Bee Egg",250000}}},
+BeachWorld={currency="Shells",unlock="150M Coins",islands={"Sea Island","Sea Rewards","Shell Isle","Oceanic Island","Sea Shell Island"},eggs={{"Coconut Egg",450},{"Sand Egg",65000},{"Beach Egg",150000},{"Balloon Egg",200000},{"Water Egg",275000},{"Crab Egg",300000}}},
+Atlantis={currency="Pearls",unlock="20M Shells",islands={"Water Island","Atlantis Rewards","Atlantis Isle","Treasure Island","Sandy Island"},eggs={{"Kelp Egg",450},{"Ocean Egg",65000},{"Darkness Egg",150000},{"Coral Egg",200000},{"Ancient Egg",300000}}},
+RainbowLand={currency="Stars",unlock="1.5B Coins",islands={"Red Island","Rainbow Rewards","Green Island","Blue Island","Purple Island"},eggs={{"Red Egg",450},{"Orange Egg",65000},{"Colorful Egg",150000},{"Fancy Egg",175000}}},
+Underworld={currency="Magma",unlock="2B Coins",islands={"Fire Island","Magma Rewards","Magma Island","Inferno Island","Molten Island"},eggs={{"Stone Egg",450},{"Obsidian Egg",65000},{"Fire Egg",150000},{"Evil Egg",175000},{"Dark Egg",175000},{"Sparkly Egg",225000}}},
+MysticForest={currency="Crystals",unlock="4B Coins",islands={"Crystal Island","Crystal Rewards","Mythic Island","Spirit Island","Magic Island"},eggs={{"Mossy Egg",450},{"Spirit Egg",65000},{"Mythical Egg",185000},{"Mushroom Egg",225000},{"Pastel Egg",300000}}}
+}
